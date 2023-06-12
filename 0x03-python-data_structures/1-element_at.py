a4a109e676b9f320e40ec8bc26db438f978e5103
ahracodes <nadififatimaezzahra01@gmail.com>
2023-06-12 09:08:53 +0100

    print -> return sub + pycodestyle fix

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0:
-        print("None")
+        return "None"
 
     elif idx > (len(my_list) + 1):
-        print("None")
+        return "None"
 
     else:
-        return(my_list[idx])
+        return my_list[idx]
