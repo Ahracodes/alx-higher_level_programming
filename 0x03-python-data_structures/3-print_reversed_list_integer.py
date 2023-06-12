54fabcf5b204ca75816f3d3808a2ab3c2522da8e
ahracodes <nadififatimaezzahra01@gmail.com>
2023-06-12 18:12:58 +0100

    task 3 empty list handling

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     idx = -1
+    if len(my_list) == 0:
+        pass
     while idx > -(len(my_list) + 1):
         print("{:d}".format(my_list[idx]))
         idx -= 1
