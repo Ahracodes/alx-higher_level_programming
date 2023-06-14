3a5d6b0eefac524bc41c41718d4e338d7f51c19b
ahracodes <nadififatimaezzahra01@gmail.com>
2023-06-14 21:58:02 +0100

    task 5 var correction

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -3,6 +3,6 @@ def number_keys(a_dictionary):
     keys_list = list(a_dictionary.keys())
     x = 0
 
-    for x in keys_list:
-        x += 1
+    for y in keys_list:
+        y += 1
     return (x)
