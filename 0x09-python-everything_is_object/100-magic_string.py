f1dbc46352928ab134cc30ee2d71608aba1dbad4
ahracodes <nadififatimaezzahra01@gmail.com>
2023-09-18 23:00:33 +0100

    delete for deferment

--- 0x09-python-everything_is_object/100-magic_string.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def magic_string():
-    magic_string.nb = getattr(magic_string, "nb", 0) + 1
-    return "BestSchool" + ", BestSchool" * (magic_string.nb - 1)
--- 0x09-python-everything_is_object/101-locked_class.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-class LockedClass:
-    def __setattr__(mod, atr, val):
-        if not hasattr(mod, atr) and atr != "first_name":
-            raise AttributeError(f"No '{atr}' attribute in 'LockedClass'")
-        super().__setattr__(atr, val)
