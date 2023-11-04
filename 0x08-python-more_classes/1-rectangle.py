e170930b2311dc9a822cd46afaf3d90605217745
ahracodes <nadififatimaezzahra01@gmail.com>
2023-11-04 17:38:53 +0100

    task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
+""" Class Rectangle """
+
 class Rectangle:
-    def __init__(self, width, height):
+    """
+    Rectangle defines rectangle by:
+    Private instance attribute: width (int)
+    Private instance attribute: height (int)
+    Instantiation with optional width and height
+    """
+
+    def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
+    @property
     def width(self):
         return self.__width
 
+    @property
     def height(self):
         return self.__height
 
+    @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError('Width must be int')
@@ -17,6 +29,7 @@ class Rectangle:
             raise ValueError('Width must be superior to 0')
         self.__width = value
 
+    @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError('height must be int')
