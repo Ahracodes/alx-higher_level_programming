160ce3e9d75c77b4e5eb7839ffb284daa41d1d2a
ahracodes <nadififatimaezzahra01@gmail.com>
2023-11-06 13:05:56 +0100

    incomplete doc task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
+"""
+Return a list of attr and methods.
+Args:
+    Obj: Object to list the methods and attr for.
 
+Returns:
+    list: list of attributes and methods associated with obj.
+"""
 def lookup(obj):
-    """
-    Return a list of attr and methods.
-
-    Args:
-        Obj: Object to list the methods and attr for.
-
-    Returns:
-        list: list of attributes and methods associated with obj.
-    """
+    """ Returns list of attrs """
     return dir(obj)
 
